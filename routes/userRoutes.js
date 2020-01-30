98d63b267f4da79d80815eb135fc04800fe563b9
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2020-01-30 19:59:15 +0700

    Second Commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/userControllers')
+const userController = require('../controllers/userController')
+const alreadyLogin = require('../middlewares/alreadyLogin')
 
 
-router.get('/', (req, res) => {
-    res.render('user/home')
-})
+router.get('/', userController.homePage)
+router.get('/logout', userController.doLogout)
+router.use('/', alreadyLogin)
+router.get('/login', userController.loginPage)
+router.post('/login', userController.doLogin)
+router.get('/register', userController.registerPage)
+router.post('/register', userController.doRegister)
 
-router.get('/login', (req, res) => {
-    res.render('user/login')
-})
-
-router.get('/register', (req, res) => {
-    res.render('user/register')
-})
 
 
 module.exports = router
