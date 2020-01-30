003d47d539f4010e15bcd454f2c8c3b4072a4314
isnirof <isnirof.ir@gmail.com>
2020-01-30 16:21:31 +0700

    2nd commit isni

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const flash = require('express-flash')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 <<<<<<< HEAD
+<<<<<<< HEAD
 const routerAdmin = require('./routes/adminRoutes')
 
+=======
+const routerAdmin = require('./routes/adminRoutes')
+const userRoutes = require('./routes/userRoutes')
+>>>>>>> 9fa6976934495543e0d5ae670a76bec86adef3a8
 app.use(express.static('assets'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.locals.formatRupiah = require('./helper/formatRupiah')
 app.locals.formatTanggal =require('./helper/formatTanggal')
+<<<<<<< HEAD
   // app.use(flash())
   // app.use(cookieParser('keyboard cat'))
   // app.use(session({
@@ -27,6 +33,8 @@ const userRoutes = require('./routes/userRoutes')
 app.use(express.static('assets'))
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
+=======
+>>>>>>> 9fa6976934495543e0d5ae670a76bec86adef3a8
 app.use(flash())
 app.use(cookieParser('keyboard cat'))
 app.use(session({
@@ -34,7 +42,10 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
   }))
+<<<<<<< HEAD
 >>>>>>> 132701d67476d2104d87f104c908095a2be1d77f
+=======
+>>>>>>> 9fa6976934495543e0d5ae670a76bec86adef3a8
 app.set('view engine', 'ejs')
 app.use('/', userRoutes)
 
